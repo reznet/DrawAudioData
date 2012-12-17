c384361b3b17be0d2f562b61b93ca0a6a4de0369
Jeff Evans <reznet@gmail.com>
2012-12-17 00:49:43 -0800

    a really big drawing, but it's kinda working!

--- DrawWaveFile/DrawWaveFile/MainWindow.xaml.cs
+++ DrawWaveFile/DrawWaveFile/MainWindow.xaml.cs
@@ -28,25 +28,104 @@ namespace DrawWaveFile
 
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
-            Attempt2();            
+            Attempt3();         
+        }
+
+        private void Attempt3()
+        {
+            byte[] data = File.ReadAllBytes(@"C:\Users\Jeff\Documents\audio files\sine 1hz 1sec signed 16bit pcm.wav");
+
+            var shorts = GetShorts(data, 44);
+
+            WriteableBitmap bitmap = BitmapFactory.New(shorts.Length, 1024);
+            imageControl.Source = bitmap;
+
+            using (var context = bitmap.GetBitmapContext())
+            {
+                bitmap.Clear(Colors.White);
+
+                int maxY = 0;
+
+                for (int i = 0; i < shorts.Length; i++)
+                {
+                    int x = i;
+                    int y = shorts[i] >> 6;
+
+                    maxY = (int)Math.Max(y, maxY);
+
+                    bitmap.SetPixel(x, y, Colors.Black);
+                }
+
+                unsafe
+                {
+                    for (int i = 0; i < shorts.Length; i++)
+                    {
+                        int x = i;
+                        int y = i % 1024;
+                        //bitmap.SetPixel(x, x % 1024, Colors.Red);
+
+                        Color color = Colors.Red;
+
+                        var a = color.A + 1;
+                        var col = (color.A << 24)
+                                 | ((byte)((color.R * a) >> 8) << 16)
+                                 | ((byte)((color.G * a) >> 8) << 8)
+                                 | ((byte)((color.B * a) >> 8));
+
+                        context.Pixels[y * context.Width + i] = col;
+                    }
+                }
+
+                Console.WriteLine("max Y:{0}", maxY);
+            }
+
+            
+        }
+
+        UInt16[] GetShorts(byte[] bytes, int offset)
+        {
+            int numberBytesToCopy = (bytes.Length - offset) / 2;
+
+            UInt16[] shorts = new UInt16[numberBytesToCopy];
+            for (int i = 0; i < numberBytesToCopy; i++)
+            {
+                byte low = bytes[offset + (i * 2)];
+                byte high = bytes[offset + ((i * 2) + 1)];
+
+                shorts[i] = (UInt16)((high << 8) + low);
+            }
+
+            return shorts;
         }
 
         private void Attempt2()
         {
-            const int width = 200;
+            const int width = 2 << 16;
             WriteableBitmap bitmap = BitmapFactory.New(width, 2 << 8);
             imageControl.Source = bitmap;
+
+            byte[] data = File.ReadAllBytes(@"C:\Users\Jeff\Documents\audio files\sine 1hz 1sec signed 16bit pcm.wav");
 
             using (bitmap.GetBitmapContext())
             {
                 bitmap.Clear(Colors.White);
 
-                for (int i = 0; i < width; i++)
+                int i = 0;
+                int x = 0;
+                while(45 + i < data.Length && i < width)
                 {
-                    int x = i;
-                    int y = 10;
+                    //Console.WriteLine("x:{0}", x);
+                    int d = data[44 + i];
+                    //Console.WriteLine("d:{0}", d);
+                    //int y = data[44 + i] << 8 + data[45 + i];
+                    int y = data[45 + i];
+
+                    i++;
+                    i++;
 
                     bitmap.SetPixel(x, y, Colors.Black);
+
+                    x++;
                 }
             }
         }
